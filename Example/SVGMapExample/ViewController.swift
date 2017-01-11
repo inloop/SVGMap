9f424440cc0a65777c77bfba491dfb1a243975f9
Tomas Srna <tomas@srna.net>
2017-01-11 16:59:33 +0100

    Initial project structure

---
+++ Example/SVGMapExample/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SVGMapExample
+//
+//  Created by Tomas Srna on 11/01/2017.
+//  Copyright © 2017 inloop. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ Source/SVGMap.h
@@ -0,0 +1,19 @@
+//
+//  SVGMap.h
+//  SVGMap
+//
+//  Created by Tomas Srna on 11/01/2017.
+//  Copyright © 2017 inloop. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for SVGMap.
+FOUNDATION_EXPORT double SVGMapVersionNumber;
+
+//! Project version string for SVGMap.
+FOUNDATION_EXPORT const unsigned char SVGMapVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SVGMap/PublicHeader.h>
+
+
